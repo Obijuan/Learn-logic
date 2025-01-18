52f5770fd26597d12d92f57bc714de6021253c6e
obijuan <obijuan.cube@gmail.com>
2025-01-18 20:15:20 +0100

    import_proof.py tool

---
+++ Metamath/import_proof.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------
+# -- Importing proofs from origina metamath to metamath.py
+#--  Src: wph wn wph wph wn id con2i
+#--  Dst: 
+#-----------------------------------------------------------
+
+proof_src = input("Introduce prueba: ")
+print(f"Prueba: {proof_src}")
+
+proof_dst = proof_src.split(" ")
+print(proof_dst)
+print()
